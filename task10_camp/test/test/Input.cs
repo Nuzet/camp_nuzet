22213bfc212f1156fad7451eaeb60223f4cc0ef9
Nuzet Abliadzhyiev <nabliadzhyiev@gmail.com>
2022-06-29 18:21:04 +0300

    task10

---
+++ task10_camp/test/test/Input.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Input
+    {
+        public static string  InputTranslation(string word)
+        {
+            Console.Write($"Введiть замiну для слова {word}: ");
+            string value = Console.ReadLine();
+            return value;
+        }
+    }
+}
---
+++ task10_camp/test/test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary;
+            try
+            {
+                Translator translator = new Translator();
+                dictionary = Service.ReadDictionary("../../../Dictionary.txt");
+                translator.AddDictionary(dictionary);
+                Service.TranslateByLine("../../../Text.txt", translator);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        
+    }
+}
